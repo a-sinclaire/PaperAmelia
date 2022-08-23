3d04e8c234915d56ec7085a4e325bb6e5f0a7fb2
Amelia Sinclaire <amelia.sylphie.sinclaire@gmail.com>
2022-08-22 22:22:08 -0400

    Added many comments

--- Scripts/PaperAmelia.py
+++ Scripts/PaperAmelia.py
@@ -5,11 +5,20 @@ import random
 from copy import deepcopy
 
 
+# scales the image (be default all my images are HUGE, so the window doesn't even fit on screen
+# this is just a func to scale imgs by some given factor so fit on my display
 def scale_img(img, scale):
     return pygame.transform.scale(img, (img.get_width() / scale, img.get_height() / scale))
 
 
+# I don't think map_range is being used rn, but it generally a useful function and i may end up using it in calc_stats
+def map_range(input, in_min, in_max, out_min, out_max):
+    slope = (out_max - out_min) / (in_max - in_min)
+    return out_min + slope * (input - in_min)
+
+
 def initialize():
+    # get path to the python script being run (this file)
     dirname = os.path.dirname(__file__)
     # read in csv of clothes data and store in pandas dataframe
     df = pd.read_csv(os.path.join(dirname, '../Assets/directory.csv'))
@@ -117,36 +126,46 @@ def shuffle(context, alt_outfit=None):
     layer_info_df = context['layer_info_df']
 
     num_layers = layer_info_df.shape[0]
-    
+
+    # shuffle the current outfit by default
     working_outfit = outfit
-    if alt_outfit is not None:
+    if alt_outfit is not None:  # otherwise shuffle the given alternate outfit
         working_outfit = alt_outfit
-    # randomize
+
+    # go through all the layers
     for l in range(num_layers):
+        # if the layer is locked, skip it
         if locked[l]:
             continue
+        # if the layer is 0 (base) skip it
         if l == 0:
             continue
+
+        # turn off all items in this layer
         copy = working_outfit[l]
         for x in range(len(copy)):
             copy[x] = False
         working_outfit[l] = copy
 
+        # pick a random item on this layer to make active
         r = random.randrange(0, num_items[l])
+        # some percent chance we leave all items in this layer off
         if random.randrange(0, num_items[l] * 10) <= 5:
             continue
         working_outfit[l][r] = True
     return working_outfit.copy()
 
 
-def minor_shuffle(context, z, k, s=None):
+def minor_shuffle(context, z, k, s=None):  # z is the layer, k is the item index, s is alt outfit
     # these won't be modified
     outfit = context['outfit']
     locked = context['locked']
     num_items = context['num_items']
 
+    # if this layer is locked, stop, don't make any changes, return current outfit
     if locked[z]:
-        return
+        return outfit
+    # operate on copt of current outfit, unless alt_outfit is passed in
     outfit_copy = deepcopy(outfit)
     if s is not None:
         outfit_copy = s
@@ -157,18 +176,18 @@ def minor_shuffle(context, z, k, s=None):
         copy[x] = False
     outfit_copy[z] = copy
 
-    # random chance to just exit (removing all items in this layer)
-    # if random.randrange(0, num_items[z]+2) <= 1:
+    # if the index we are going to turn on is invalid range, return current outfit with this layer turned completely off
+    # this is used. sometimes we pass in the index +1 so that we can see if no items improves the score more
+    # than any real item in the list
     if k > num_items[z] - 1:
         return outfit_copy
 
-    # choose a random item to turn on
-    # r = random.randrange(0, num_items[z])
+    # otherwise we return the outfit with the selected item turned on (and that is all that is on in the given layer)
     outfit_copy[z][k] = True
     return outfit_copy
 
 
-def major_optimize(context, drawing_context, optimization_context, axis, max_iterations, max=True):
+def major_optimize(context, drawing_context, optimization_context, max_iterations):
     # context
     outfit = context['outfit']
     active_item = context['active_item']
@@ -183,36 +202,42 @@ def major_optimize(context, drawing_context, optimization_context, axis, max_ite
     OP_AXIS = optimization_context["OP_AXIS"]
     OP_AXIS_MAX = optimization_context["OP_AXIS_MAX"]
 
+    # draw red circle in corner to indicate processing
     pygame.draw.circle(screen, (255, 0, 0), (WIDTH - 20, 20), 10)
     pygame.display.update()
 
     # get score for current outfit / set it to max score / save current outfit
     stats = calc_stats(context)
-    max_score = stats[axis]
+    max_score = stats[OP_AXIS]
 
     save_outfit = deepcopy(outfit)
+
+    # we will randomly shuffle the outfit 'max_iterations' times
     for i in range(max_iterations):
         # get new random outfit / score it
         new_outfit = shuffle(context, deepcopy(outfit))
         stats = calc_stats(context, new_outfit)
-        new_score = stats[axis]
-        # if new score is better, new score is new max score / save this outfit as current best
-        if max:
+        new_score = stats[OP_AXIS]
+        # if new score is better than what we had previously
+        # then we update the current outfit to be that new 'best outfit'
+        # and we break out. stopping all further iterations
+        # (we only find the next random outfit that is better than what we currently have)
+        if OP_AXIS_MAX:  # if trying to maximize the score
             if new_score > max_score:
-                max_score = new_score
                 save_outfit = deepcopy(new_outfit)
                 outfit = deepcopy(save_outfit)
                 context['outfit'] = outfit
+                # draw new best outfit
                 display(context, drawing_context)
                 pygame.draw.circle(screen, (255, 0, 0), (WIDTH - 20, 20), 10)
                 pygame.display.update()
                 break
-        else:
+        else:  # if trying to minimize
             if new_score < max_score:
-                max_score = new_score
                 save_outfit = deepcopy(new_outfit)
                 outfit = deepcopy(save_outfit)
                 context['outfit'] = outfit
+                # draw new best outfit
                 display(context, drawing_context)
                 pygame.draw.circle(screen, (255, 0, 0), (WIDTH - 20, 20), 10)
                 pygame.display.update()
@@ -230,6 +255,13 @@ def major_optimize(context, drawing_context, optimization_context, axis, max_ite
 
 
 def multi_opt(context, drawing_context, optimization_context, axis_array, iterations):
+    # this whole function is sort of a WIP (it's not used anywhere)
+    # the idea is to be able to optimize on multiple axes
+    # basically it only modifies one random piece of clothing at a time, on a specific axis determined by
+    # the axis_weights array (higher weights mean that axis is chosen more often)
+    # if we do this enough times, then hopefully the random alterations will approach stats with agreeable values
+    # taht is the idea anyway
+
     # axis_key = ['total_coverage', 'weight', 'avg_thickness', 'avg_breathability', 'waterproof', 'avg_brightness', 'sportiness', 'formality', 'loungeablity', 'warmth']
     # ex. winter condition
     # axis_array_weights = [0.9, 0.5, 0.8, 0.4, 0.2, 0.6, 0.1, 0.0, 0.15, 0.99]
@@ -241,11 +273,13 @@ def multi_opt(context, drawing_context, optimization_context, axis_array, iterat
     index_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in range(round(iterations)):
         ind = random.choices(index_array, weights=axis_array_weights, k=1)
-        context = minor_optimize(context, drawing_context, optimization_context, index_array[ind[0]], 1, axis_array_maxs[ind[0]], rand=True)
+        optimization_context['OP_AXIS'] = index_array[ind[0]]
+        optimization_context['OP_AXIS_MAX'] = axis_array_maxs[ind[0]]
+        context = minor_optimize(context, drawing_context, optimization_context, 1, rand=True)
     return context
 
 
-def minor_optimize(context, drawing_context, optimization_context, axis, iterations, max=True, rand=False):
+def minor_optimize(context, drawing_context, optimization_context, iterations, rand=False):
     # context
     outfit = context['outfit']
     active_item = context['active_item']
@@ -264,55 +298,75 @@ def minor_optimize(context, drawing_context, optimization_context, axis, iterati
     OP_AXIS_MAX = optimization_context["OP_AXIS_MAX"]
 
     num_layers = layer_info_df.shape[0]
-
+    
+    # we draw a small blue indicator in the corner of the screen to indicate the program is processing
     pygame.draw.circle(screen, (0, 0, 255), (WIDTH - 20, 40), 10)
     pygame.display.update()
 
-    # get score for current outfit / set it to max score / save current outfit
+    # get metrics for current outfit
     stats = calc_stats(context)
-    max_score = stats[axis]
+    prev_score = stats[OP_AXIS]  # save the score on the field we are interested in optimizing (prev best)
 
     similarity_counter = 0
 
     # original outfit stored in save_outfit
     save_outfit = deepcopy(outfit)
+    
+    # we loop through each item on each layer to see if any one item can provide a better score
+    # if an item improves the score we save that as new prev best, and update current outfit to be the 'winning outfit'
+    # continue for given number of iterations
+    # (we go through the layers in order, and the items on each layer in order, but
+    # always start in random spots in those lists)
     for i in range(iterations):
-        # find minor improvements randomly
         # operate on 'next layer', try next article of clothing
         layer_op = (i % (num_layers - 1)) + 1
-        if rand:
+        if rand:  # if rand we operate on a completely random layer
             r = random.randrange(0, (num_layers - 1))
             layer_op = ((i+r) % (num_layers - 1)) + 1
         item_op_offset = random.randrange(0, num_items[layer_op] + 1)
+
+        # go through each item on this layer
         for j in range(num_items[layer_op] + 1):
             item_op = ((j+item_op_offset) % (num_items[layer_op] + 1))
+
+            # minor shuffle doesn't really shuffle
+            # it just turns off everything in this layer, then turns on the specified item index (item_op)
             new_outfit = minor_shuffle(context, layer_op, item_op, deepcopy(outfit))
             stats = calc_stats(context, new_outfit)
-            new_score = stats[axis]
-            # if new score is better, new score is new max score / save this outfit as current best
-            if max:
-                if new_score > max_score:
-                    max_score = new_score
+            new_score = stats[OP_AXIS]
+
+            # if new score is better, new score is prev best / save this outfit as new current outfit
+            if OP_AXIS_MAX:  # if we are trying to maximize the score
+                if new_score > prev_score:
+                    prev_score = new_score
                     save_outfit = deepcopy(new_outfit)
                     outfit = deepcopy(new_outfit)
                     context['outfit'] = outfit
+                    # draw this new better outfit on screen
                     display(context, drawing_context)
                     pygame.draw.circle(screen, (0, 0, 255), (WIDTH - 20, 40), 10)
                     pygame.display.update()
-            else:
-                if new_score < max_score:
-                    max_score = new_score
+            else:  # if we are trying to minimize the score
+                if new_score < prev_score:
+                    prev_score = new_score
                     save_outfit = deepcopy(new_outfit)
                     outfit = deepcopy(new_outfit)
                     context['outfit'] = outfit
+                    # draw this new better outfit on screen
                     display(context, drawing_context)
                     pygame.draw.circle(screen, (0, 0, 255), (WIDTH - 20, 40), 10)
                     pygame.display.update()
+
+        # if our current outfit is the same as the previous outfit add 1 to the similarity counter
         if outfit == save_outfit:
             similarity_counter += 1
         else:
-            similarity_counter = 0
+            similarity_counter = 0  # if it differs, reset similarity to 0
+
+        # if the similarity counter reaches a certain threshold (we have had the same outfit for so many iterations
+        # without any improvements) then we stop searching and say we are optimized
         if similarity_counter >= num_layers * 3:
+            # this stuff (for loop) just sets the active items to be the same as what is on the current outfit
             for l in range(len(outfit)):
                 for a in range(len(outfit[l])):
                     if outfit[l][a]:
@@ -321,31 +375,33 @@ def minor_optimize(context, drawing_context, optimization_context, axis, iterati
             return context
     # assign the best found outfit to be current outfit
     outfit = deepcopy(save_outfit)
+    # this stuff (for loop) just sets the active items to be the same as what is on the current outfit
     for l in range(len(outfit)):
         for a in range(len(outfit[l])):
             if outfit[l][a]:
                 active_item[l] = a
                 context['outfit'] = outfit
+
+    # save the outfit we found to be the current outfit in the context
     context['outfit'] = outfit
     return context
 
 
-def map_range(input, in_min, in_max, out_min, out_max):
-    slope = (out_max - out_min) / (in_max - in_min)
-    return out_min + slope * (input - in_min)
-
-
 def calc_stats(context, alt_outfit=None):
-    df = context['dataframe']
+    # context
     # these won't be modified
+    df = context['dataframe']
     outfit = context['outfit']
     layer_info_df = context['layer_info_df']
 
     num_layers = layer_info_df.shape[0]
-    
+
+    # we calculate the stats of out current outfit by default
     working_outfit = outfit
-    if alt_outfit is not None:
+    if alt_outfit is not None:  # if an alternate outfit is passed in we calculate its stats instead
         working_outfit = alt_outfit
+
+    # initializing stats
     n_clothes = 0
     total_coverage = 0
     weight = 0
@@ -356,12 +412,12 @@ def calc_stats(context, alt_outfit=None):
     sportiness = 0
     formality = 0
     loungeablity = 0
-
     warmth = 0
+
     for l in range(num_layers):
         items_in_layer = df[df.layer == l]
         for i in range(len(working_outfit[l])):
-            if working_outfit[l][i]:
+            if working_outfit[l][i]:  # if current item we are looking at is turned ON
                 n_clothes += 1
                 # get stats on this article
                 row = items_in_layer.iloc[i]
@@ -382,11 +438,10 @@ def calc_stats(context, alt_outfit=None):
                 avg_waterproofing += coverage * waterproofing
                 avg_brightness += coverage * row['brightness']
                 warmth += coverage * ((thickness + (1.0 - breathability) + (-avg_brightness*0.30)) / 3)
+    # do some wierd math, idk, should prob think about this more
     if num_layers != 0 and n_clothes != 0:
         avg_thickness = (avg_thickness / (num_layers - 1)) * 100.00
         avg_breathability = 100.00 - ((avg_breathability / (num_layers-1)) * 100.00)
-        # avg_waterproofing = (avg_waterproofing / (num_layers-1)) * 100.00
-        # avg_brightness = (avg_brightness / (num_layers-1)) * 100.00
         sportiness = (sportiness / (num_layers-1)) * 100.00
         formality = (formality / (num_layers-1)) * 100.00
         loungeablity = (loungeablity / (num_layers-1)) * 100.00
@@ -394,22 +449,13 @@ def calc_stats(context, alt_outfit=None):
         avg_waterproofing = (100.00 * avg_waterproofing) / total_coverage
         avg_brightness = (100.00 * avg_brightness) / total_coverage
         warmth = (100.00 * warmth) / total_coverage
-    # avg_brightness *= 100.00
-    # sportiness *= 100.00
-    # formality *= 100.00
-    # loungeablity *= 100.0
+    # should probably return this as a dictionary
     return (
     total_coverage, weight, avg_thickness, avg_breathability, avg_waterproofing, avg_brightness, sportiness, formality,
     loungeablity, warmth)
 
 
 def display(context, drawing_context, alt_outfit=None):
-    """
-    :param context: df, outfit, num_items
-    :param drawing_context: screen
-    :param temp_outfit: optional - can specify an outfit other than the active one to draw
-    :return: None
-    """
     # context
     df = context['dataframe']
     # these won't be modified
@@ -707,10 +753,10 @@ def check_events(context, overlay_toggles, drawing_context, optimization_context
                 context['active_item'] = active_item
             # optimize on the axis randomly
             if event.key == pygame.K_m:
-                context = major_optimize(context, drawing_context, optimization_context, OP_AXIS, 1000, OP_AXIS_MAX)
+                context = major_optimize(context, drawing_context, optimization_context, 1000)
             # optimize on the axis systematically
             if event.key == pygame.K_n:
-                context = minor_optimize(context, drawing_context, optimization_context, OP_AXIS, 300, OP_AXIS_MAX)
+                context = minor_optimize(context, drawing_context, optimization_context, 300)
             # lock the current layer
             if event.key == pygame.K_l:
                 locked[active_layer] = True
@@ -776,8 +822,3 @@ if __name__ == '__main__':
 
         pygame.display.update()
         clock.tick(FPS)
-
-# context = df, outfit, active_layer, active_item, num_items, locked, dirname
-# optimization_context = OP_AXIS, OP_AXIS_MAX
-# drawing_context = screen, clock, FPS, SCALE, BACKGROUND_IMG, WIDTH, HEIGHT, my_font, layer_info_df, axis_key
-# overlay_toggles = SHOW_GUI, SHOW_HELP, SHOW_STATS
