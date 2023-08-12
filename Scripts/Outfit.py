f85b6003d6990b1f21b30f87651a0515d748aaed
Amelia Sinclaire <amelia.sylphie.sinclaire@gmail.com>
2023-08-12 18:38:49 -0400

    Priorities added based on order in which the assets are loaded.
    Always draw outfits in order of layer then priority.

--- Scripts/Outfit.py
+++ Scripts/Outfit.py
@@ -10,19 +10,20 @@ class Article:
     csv_titles = ''
     articles = []
     asset_path = ''
+    scale = 4.0
+    highest_priority = -1
 
-    def __init__(self, layer, file_path, csv_data, priority=0, scale=4.0):
+    def __init__(self, layer, file_path, csv_data, priority=0):
         self.layer = layer
         self.file_path = file_path
         self.csv_data = csv_data
         self.priority = priority
         self.sprite = None
-        self.scale = scale
         Article.articles.append(self)
 
     def load_sprite(self):
         self.sprite = pygame.image.load(self.file_path)
-        self.sprite = scale_img(self.sprite, self.scale)
+        self.sprite = scale_img(self.sprite, Article.scale)
         print(f'LOADED: {self.file_path}')
 
     @staticmethod
@@ -44,8 +45,8 @@ class Article:
             lines = f.readlines()
 
         Article.csv_titles = lines[0]
-        for line in lines[1:]:
-            Article.load_article(line)
+        for csv_line in lines[1:]:  # skip csv header
+            Article.load_article(csv_line)
 
     @staticmethod
     def load_article(csv_line):
@@ -56,20 +57,30 @@ class Article:
         layer = properties[0]
         article_file_path = os.path.join(Article.asset_path, properties[1])
 
-        return Article(layer, article_file_path, csv_line)
+        Article.highest_priority += 1
+        return Article(layer, article_file_path, csv_line, priority=Article.highest_priority)
 
 
 class Outfit:
     def __init__(self, articles=[]):
         self.articles = articles
+        self.sorted = False
 
     def toggle_article(self, article):
+        self.sorted = False
         if article in self.articles:
             self.articles.remove(article)
             return
         self.articles.append(article)
 
+    def sort(self):
+        self.articles.sort(key=lambda a: (a.layer, a.priority))
+        self.sorted = True
+
     def draw(self, screen):
+        if not self.sorted:
+            self.sort()
+
         for a in self.articles:
             a.draw(screen)
 
@@ -85,7 +96,7 @@ class Outfit:
         with open(file_path, 'r') as f:
             lines = f.readlines()
         for line in lines[1:]:  # first line has header data for *some reason*
-            article = Article.search(line)
+            article = Article.search(line)  # check if article is already in Article list
             if article is None:
                 article = Article.load_article(line)
             self.toggle_article(article)
